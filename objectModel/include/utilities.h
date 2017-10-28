0149545c26a64ac1eb00fea77631baf5fc005e20
simharry3 <simharry3@gmail.com>
2017-10-28 17:07:12 -0400

    Added utilities files

---
+++ objectModel/include/utilities.h
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <unistd.h>
+#include <stdint.h>
+#include <algorithm>
+
+uint32_t expandBits(unsigned int );
+
+uint64_t encodePosition(float, float, float);
+
+#endif
---
+++ objectModel/src/utilities.cpp
@@ -0,0 +1,21 @@
+#include "utilities.h"
+
+using namespace std;
+
+uint32_t expandBits(unsigned int v){
+    v = (v * 0x00010001u) & 0xFF0000FFu;
+    v = (v * 0x00000101u) & 0x0F00F00Fu;
+    v = (v * 0x00000011u) & 0xC30C30C3u;
+    v = (v * 0x00000005u) & 0x49249249u;
+    return v;
+}
+
+uint64_t encodePosition(float x, float y, float z){
+    x = min(max(x * 1024.0f, 0.0f), 1023.0f);
+    y = min(max(y * 1024.0f, 0.0f), 1023.0f);
+    z = min(max(z * 1024.0f, 0.0f), 1023.0f);
+    unsigned int xx = expandBits((unsigned int)x);
+    unsigned int yy = expandBits((unsigned int)y);
+    unsigned int zz = expandBits((unsigned int)z);
+    return xx * 4 + yy * 2 + zz;
+}
