759384ecc9f243637b5eb7880b2e89f632ed3d43
simharry3 <simharry3@gmail.com>
2017-10-29 18:19:44 -0400

    updates to paper

--- objectModel/src/simulation.cpp
+++ objectModel/src/simulation.cpp
@@ -9,7 +9,6 @@ int main(int argc, char* argv[]){
     Universe* u1 = new Universe(atoi(argv[1]));
     u1->addParticles(atoi(argv[2]));
     int center[3] = {3, 3, 3};
-    // u1.addAggregators((char*)"aggregators.dat");
     u1->generateAggregators(1, 1, center);
     const int* bounds = u1->getBounds();
     center[0] = bounds[0] - 3;
@@ -22,15 +21,9 @@ int main(int argc, char* argv[]){
     
     int step = 0;
     while(u1->getNumActiveParticles() > atoi(argv[2]) * .1){
-        // clock_t start = clock();
         u1->generateMortonCodes();
         u1->moveParticles();
         ++step;
-        // clock_t end = clock();
-        // printf("%fms\n", (double)(end-start)*1000.0/CLOCKS_PER_SEC);
-        // printf("STEP %d | ACTIVE: %d | AGGREGATORS: %d\n", step, u1->numParticles, u1->numAggregators);
-        // u1.writeOutputFile((char*)"output.dat");
-        // sleep(1);
     }
     delete u1;
 }
