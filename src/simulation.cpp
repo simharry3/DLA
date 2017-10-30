3ff9db23ee00a97efa23130dc39f851dd535c230
simharry3 <simharry3@gmail.com>
2017-10-29 21:35:50 -0400

    Added ability to input aggregator file

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -2,19 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 // #include <unistd.h>
-int numParticles = 1000;
-int universeSize = 600;
 
 int main(int argc, char* argv[]){
+    if(argc < 3){
+        printf("USAGE: <dir>/bin/simulation <Size> <Num Particles> <aggregator.txt(optional)>\n");
+        return EXIT_FAILURE;
+    }
     Universe* u1 = new Universe(atoi(argv[1]));
     u1->addParticles(atoi(argv[2]));
-    int center[3] = {3, 3, 3};
-    u1->generateAggregators(1, 1, center);
-    const int* bounds = u1->getBounds();
-    center[0] = bounds[0] - 3;
-    center[1] = bounds[1] - 3;
-    center[2] = bounds[2] - 3;
-    u1->generateAggregators(1, 1, center);
+    if(argc == 3){
+        int center[3] = {3, 3, 3};
+        u1->generateAggregators(1, 1, center);
+        const int* bounds = u1->getBounds();
+        center[0] = bounds[0] - 3;
+        center[1] = bounds[1] - 3;
+        center[2] = bounds[2] - 3;
+        u1->generateAggregators(1, 1, center);
+    }
+   else if(argc > 3){
+        u1->addAggregators(argv[3]);
+   }
     u1->reserveMemory();
 
     u1->renderUniverse();
@@ -26,4 +33,5 @@ int main(int argc, char* argv[]){
         ++step;
     }
     delete u1;
+    return EXIT_SUCCESS;
 }
